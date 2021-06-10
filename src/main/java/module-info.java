4a89854de9f19d7f3564b1db687cb298a1bcf4a0
willi-z <willi.w.zschiebsch@web.de>
2021-06-10 16:07:40 +0200

    gradle

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,9 +4,6 @@ module msys.client {
     requires org.apache.httpcomponents.httpclient;
     requires org.apache.httpcomponents.httpcore;
     requires org.apache.commons.codec;
-    // requires org.kordamp.iconli.core;
-    // requires org.kordamp.ikonli.javafx;
-    // requires org.kordamp.ikonli.fontawesome;
     requires  Java.WebSocket;
 
     exports msys.client;
