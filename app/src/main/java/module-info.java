83fff6e6fca6951274e80b8625ca5f653b2ce0b1
willi-z <willi.w.zschiebsch@web.de>
2021-06-10 11:09:49 +0200

    workflow

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -1,12 +1,15 @@
-module msys_client {
+module msys {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.google.gson;
     requires org.apache.httpcomponents.httpclient;
     requires org.apache.httpcomponents.httpcore;
     requires org.apache.commons.codec;
+    requires org.kordamp.iconli.core;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.fontawesome;
     //requires javax.websocket.client.api;
 
-    opens msys.client to javafx.fxml;
+    //opens msys.client to javafx.fxml;
     exports msys.client;
 }
